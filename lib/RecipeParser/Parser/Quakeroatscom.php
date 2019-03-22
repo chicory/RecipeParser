7d8933c755a045506ab2b5f4b64c2a874f2ba051
Eric C. Chamberlain <ericcchamberlain@gmail.com>
2019-03-22 12:19:32 -0400

    Add Image URL to Quaker scraper

--- lib/RecipeParser/Parser/Quakeroatscom.php
+++ lib/RecipeParser/Parser/Quakeroatscom.php
@@ -13,6 +13,26 @@ class RecipeParser_Parser_Quakeroatscom {
             $recipe->title = trim($nodes->item(0)->nodeValue);
         }
 
+        /** Recipe Image structure
+         *
+         * <div id="cphStaticMaster_cpContent1_C001_divWithoutVideo" class="container-fluid trpl-cklt"
+         * style="background-image:url('/images/default-source/RecipeModule/banana-nut-overnight-oats-desktop');position:relative;">
+         */
+        // Recipe Image
+        $nodes = $xpath->query('//div[@id="cphStaticMaster_cpContent1_C001_divWithoutVideo"]');
+        if ($nodes->length) {
+            $recipe_style = $nodes->item(0)->getAttribute('style');
+            $re = '/(?<=background-image:url\(\')(.*)(?=\')/m';
+            preg_match($re, $recipe_style, $match, PREG_OFFSET_CAPTURE, 0);
+            if ($match) {
+                if (strpos( $url, 'stage.quakeroats.com' ) !== false) {
+                    $recipe->photo_url = trim('https://stage.quakeroats.com' . $match[0][0]);
+                } else {
+                    $recipe->photo_url = trim('https://www.quakeroats.com' . $match[0][0]);
+                }
+            }
+        }
+
         // Servings
         $nodes = $xpath->query('//span[@id="cphStaticMaster_cpContent1_C001_lblServings"]');
         if ($nodes->length) {
@@ -26,8 +46,6 @@ class RecipeParser_Parser_Quakeroatscom {
                 $line = trim($node->nodeValue);
                 $recipe->appendIngredient($line);
         }
-
-
 
         return $recipe;
     }
